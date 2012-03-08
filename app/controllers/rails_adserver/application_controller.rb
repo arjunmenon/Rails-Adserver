5aee7bb831e30f95c3d3ca96e06bca07b52f5f67
Thomas Muntaner <thomas.muntaner@gmail.com>
2012-03-07 19:56:10 -0600

    fixed app controller

--- app/controllers/rails_adserver/application_controller.rb
+++ app/controllers/rails_adserver/application_controller.rb
@@ -1,5 +1,5 @@
 module RailsAdserver
-  class ApplicationController < ActionController
+  class ApplicationController < ActionController::Base
     helper_method :_current_user
     helper_method :_application_name
     helper_method :_model_pool
